3fc357b18cce4dbc8673437a4d4db5b85484712c
dhakegaurang <dhakegaurang.com>
2018-07-18 21:19:19 -0400

    Implemented FileProcessor basic methods

--- maxKVisitors/src/maxKVisitors/util/FileProcessor.java
+++ maxKVisitors/src/maxKVisitors/util/FileProcessor.java
@@ -1,5 +1,29 @@
 package maxKVisitors.util;
 
-public class FileProcessor {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class FileProcessor {
+	private String inputFilePath;
+	private BufferedReader bReaderObj;
+	
+	public FileProcessor(String inputFilePath) {
+		this.inputFilePath = inputFilePath;
+	}
+	
+	public void readLine() {
+		try{
+			bReaderObj = new BufferedReader(new FileReader(inputFilePath));
+		    String line;
+		    while ((line = bReaderObj.readLine()) != null) {
+		       String[] dataArray = line.split(":");
+		    }
+		}
+		catch(IOException  e) { //handling in case of bufferedReader goes wrong
+			System.err.println("Exception: while reading input file");
+			System.out.println("Exiting...");
+			System.exit(0);
+		}
+	}
 }
