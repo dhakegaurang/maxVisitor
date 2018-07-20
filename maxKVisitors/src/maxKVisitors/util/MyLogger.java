c2d9442238282e014f29403534745f17bcdcaa28
dhakegaurang <dhakegaurang.com>
2018-07-19 23:04:59 -0400

    Implemented basic Logger class and added new levels in it

---
+++ maxKVisitors/src/maxKVisitors/util/MyLogger.java
@@ -0,0 +1,53 @@
+package maxKVisitors.util;
+
+/**
+* Logger program helps in keeping track of program run
+*
+* @author  Gaurang Dhake
+* @version 1.0
+* @since   07/19/18 
+*/
+public class MyLogger{
+    // These are debugLevel values which represents states or classes
+    public static enum DebugLevel { FILE_PROCESSOR, NONE, MAX_HEAP_VISITOR, MODIFIED_BUBBLE_SORT_VISITOR,
+    								RESULTS, MY_ARRAY, MY_VECTOR
+                                   };
+    
+    private static DebugLevel debugLevel; // static current debugLevel for the program
+    
+    /**
+	   * This is method which assigns current debugLevel using command line argument
+	   * @param levelIn this parameter decides which debugLevel to set from cmd
+	   * @return DebugLevel returns debugLevel which was set here
+	   */
+    public static void setDebugValue (int levelIn) {
+		switch (levelIn) {
+			case 1: debugLevel = DebugLevel.FILE_PROCESSOR; break;
+			case 2: debugLevel = DebugLevel.MAX_HEAP_VISITOR; break;
+			case 3: debugLevel = DebugLevel.MODIFIED_BUBBLE_SORT_VISITOR; break;
+			case 4: debugLevel = DebugLevel.RESULTS; break;
+			case 5: debugLevel = DebugLevel.MY_ARRAY; break;
+			case 6: debugLevel = DebugLevel.MY_VECTOR; break;
+			default: debugLevel = DebugLevel.NONE; break;
+		}
+		
+    }
+    
+    /**
+	   * This is method which writes message incoming from respective class
+	   * @param message this is the message to be printed
+	   * @param levelIn to check whether current level is the level being passed
+	   * @return None since return type is void
+	   */
+    public static void writeMessage (String message, DebugLevel levelIn ) {
+		if (levelIn == debugLevel)
+		    System.out.println(message);
+    }
+    /**
+	   * This is method returns string containing debugLevel
+	   * @return String it returns string for debugLevel
+	   */
+    public String toString() {
+    	return "The debug level has been set to the following " + debugLevel;
+    }
+}
